6483689851be7d077140f86258dff09dad3984e1
Aaryan-tech-otu <aaryan.kulkarni@ontariotechu.net>
2026-03-06 15:39:59 -0500

    Fix formatting of gamesList array in FrontEnd.java

--- FrontEnd.java
+++ FrontEnd.java
@@ -9,7 +9,7 @@ import java.nio.channels.ScatteringByteChannel;
 public class FrontEnd {
 
     private JFrame frame;
-    private String[] gamesList = {"Game1", "game2", "Game3", "Game4", "Game 5", "Game 6", "Game 7", "Game 8", "Game 9", "Game 10", "Game 11", "Game 12", "Game 13", "Game 14"};
+    private String[] gamesList = {"Game 1", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6", "Game 7", "Game 8", "Game 9", "Game 10", "Game 11", "Game 12", "Game 13", "Game 14"};
 
     public FrontEnd(){
         createLoginScreen();
