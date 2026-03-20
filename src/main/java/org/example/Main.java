1d52ff826bdd42cb28cc3cf70fb300901a0a6143
ZayanK777 <zayan.khan1@ontariotechu.net>
2026-03-20 13:10:51 -0400

    Added server for web. Transfered front end into Javascript. Implemented frontend with backend. Genres.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    private static final int PORT = 6789;
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+        server.createContext("/", (HttpExchange exchange) -> {
+            try {
+                File htmlFile = new File("src/main/resources/index.html");
+                System.out.println("Reading: " + htmlFile.getAbsolutePath());
+
+                byte[] htmlBytes = java.nio.file.Files.readAllBytes(htmlFile.toPath());
+                System.out.println("Bytes read: " + htmlBytes.length);
+
+                exchange.getResponseHeaders().set("Content-Type", "text/html; charset=utf-8");
+                exchange.sendResponseHeaders(200, htmlBytes.length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(htmlBytes);
+                os.flush();
+                os.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        server.start();
+        System.out.println("CLOUD5 started → http://localhost:" + PORT);
+        openBrowser("http://localhost:" + PORT);
+    }
+
+    private static void openBrowser(String url) {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("win")) {
+                Runtime.getRuntime().exec(new String[]{"rundll32", "url.dll,FileProtocolHandler", url});
+            } else if (os.contains("mac")) {
+                Runtime.getRuntime().exec(new String[]{"open", url});
+            } else {
+                Runtime.getRuntime().exec(new String[]{"xdg-open", url});
+            }
+        } catch (IOException e) {
+            System.out.println(url);
         }
     }
 }
